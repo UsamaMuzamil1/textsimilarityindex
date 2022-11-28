a2c4cfd98bf636087d47fcffe4ac4fbe5aad3c84
UsamaMuzamil1 <107238257+UsamaMuzamil1@users.noreply.github.com>
2022-11-28 13:16:03 +0500

    Update simliarityindex.py
    I can add additional comments in the script that gives more clarity in each step

--- simliarityindex.py
+++ simliarityindex.py
@@ -4,19 +4,34 @@ import pandas as pd
 import openpyxl
 
 
-
+# in this step I can load excel file to read data in it.
 df =pd.read_excel("sim_index.xlsx")
 
+
+# now , I can create function that is used to compare set of predicted labels
 def jaccard_similarity(a, b):
     a = set(a)
     b = set(b)
-    # calucate jaccard similarity
+
+
+# formula to calucate jaccard similarity
     return float(len(a.intersection(b))) / len(a.union(b))
 
+
+
+# then I can calculate the best match of column 1 which is available in column 2.
 df['best_match'] = [x for x in df['RealName'].str.lower() for x in get_close_matches(x, df['Name'].str.lower())]
+
+
+# now I can measure the similarity between two columns, with a range from 0% to 100%
 df['similarity_index'] = df.apply(lambda x: SequenceMatcher(None, x['RealName'].lower(), x['best_match']).ratio()*100, axis=1)
+
+
+# prin the results 
 print(df)
 
+
+# output can be stored in new excel file
 df.to_excel("saveindexsimilarity.xlsx")
 
 

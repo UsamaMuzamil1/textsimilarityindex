f303f8b1d584562a936836a2dc65d600cceb8421
UsamaMuzamil1 <107238257+UsamaMuzamil1@users.noreply.github.com>
2022-12-14 10:15:53 +0500

    Update index.py
    function to find text similarity

--- index.py
+++ index.py
@@ -1,26 +1,14 @@
-from difflib import get_close_matches, SequenceMatcher
-from math import sqrt, pow, exp
-import embeddings as embeddings
-import nlp as nlp
-import numpy as np
 import pandas as pd
-import openpyxl
+import numpy as np
 
+# read excel file
+similar = pd.read_excel('sim_index.xlsx')
 
-# in this step I can load excel file to read data in it.
-df =pd.read_excel("sim_index.xlsx")
+# compare column1 and column2  (1 means similar and 0 means not similar)
+similar_col = np.where(similar.Name == similar.RealName, 1,0)*100
 
+# create new column and find similarity
+similar['Similarity'] = similar_col
 
-# then I can calculate the best match of column 1 which is available in column 2.
-df['best_match'] = [x for x in df['RealName'].str.lower() for x in get_close_matches(x, df['Name'].str.lower())]
-
-
-# now I can measure the similarity between two columns, with a range from 0% to 100%
-df['similarity_index'] = df.apply(lambda x: SequenceMatcher(None, x['RealName'].lower(), x['best_match']).ratio()*100, axis=1)
-
-
-# apply function
-# df['Similarity'] = df.apply(lambda x: x['Name'] if x['Name'] <= x['RealName']  else np.nan, axis=1)
-
-# printing the dataframe
-print(df)
+# print the result
+print(similar)
